f79cffbae137c5f710c712d18066d708cbd80c30
MD RAKIBUL ISLAM RAKIB <rakibulinux@gmail.com>
2023-04-05 15:11:05 +0600

    adding route to check

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ObjectId } = require("mongodb");
+
+// Mongo Connection URL
+const url = "mongodb://localhost:27017/";
+
+// Database Name
+const dbName = "binary-tree-db";
+
+app.get("/search/:startNodeId", async (req, res) => {
+  const startNodeId = req.params.startNodeId;
+
+  try {
+    const client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+
+    // Get the starting node from the database
+    const startNode = await db
+      .collection("nodes")
+      .findOne({ _id: ObjectId(startNodeId) });
+
+    // Initialize the queue with the starting node
+    const queue = [startNode];
+    const visited = [];
+
+    // Loop until all nodes have been visited
+    while (queue.length > 0) {
+      // Dequeue the next node from the front of the queue
+      const currentNode = queue.shift();
+
+      // Mark the current node as visited
+      visited.push(currentNode);
+
+      // Enqueue the current node's children (if they exist)
+      if (currentNode.left) {
+        const leftChild = await db
+          .collection("nodes")
+          .findOne({ _id: currentNode.left });
+        queue.push(leftChild);
+      }
+      if (currentNode.right) {
+        const rightChild = await db
+          .collection("nodes")
+          .findOne({ _id: currentNode.right });
+        queue.push(rightChild);
+      }
+    }
+
+    res.json({ visitedNodes: visited });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
